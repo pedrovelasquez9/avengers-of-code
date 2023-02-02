e0bd2eab2f94848339736de573d4a39d7b0ef2ba
altaskur <altaskur@gmai.com>
2023-02-03 00:28:42 +0100

    Add Streamers Data

---
+++ src/data/stremers.ts
@@ -0,0 +1,116 @@
+interface StreamerData {
+    name : String,
+    web: String,
+    GitHub: String,
+    YouTube: String,
+    specialty: string[],
+    avatar: String,
+    description: String;
+}
+
+const streamers: StreamerData[] = [
+    {
+        name: "danirod_",
+        web:"https://danirod.es/",
+        GitHub: "https://github.com/danirod",
+        specialty: ["@makingas","Master de 🥝"],
+        YouTube: "https://www.youtube.com/makigas",
+        avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/5ca495d6-ad33-449d-baf4-831f1e2c02f5-profile_image-150x150.png",
+        description: "Desarrollador de Cobol,padre de pinchito, paginador de años"
+    },
+    {
+        name: "raupulus",
+        web:" https://fryntiz.es",
+        GitHub:"https://github.com/fryntiz",
+        specialty: ["Iot" , "php", "laravel"],
+        YouTube: "https://www.youtube.com/@raupulus",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/8eab6fe3-f74e-4a99-849c-c3e3876a499d-profile_image-150x150.png",
+        description: "Desarrollador de Iot, php y laravel",
+    },
+    {
+        name: "Ikurotime",
+        web:" https://www.davidhuertas.dev/",
+        GitHub:"github.com/ikurotime",
+        specialty: ["Desarrollo web", "Frontend"],
+        YouTube: "https://www.youtube.com/channel/UCFWJHnaItB4ueAm14lcRDVw",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/d2b75368-cf25-44ee-adfd-80f65b2da69b-profile_image-150x150.png",
+        description: "Desarrollador frontend, conocedor de mucho, maestro de ninguna",
+    },
+    {
+        name: "programacion_es",
+        web:"https://programacion-es.dev/",
+        GitHub:"https://github.com/pedrovelasquez9",
+        specialty: ["Desarrollo web","Tocador de barba"],
+        YouTube: "https://www.youtube.com/c/programaci%C3%B3n%20en%20espa%C3%B1ol",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/731610a2-828b-47d7-b3c1-3f7d14e27775-profile_image-150x150.png",
+        description: "poseedor de la fuerza, quemador de sables",
+    },
+    {
+        name: "altaskur",
+        web:"https://altaskur.github.io/",
+        GitHub:"https://github.com/altaskur",
+        specialty: ["Desarrollo web","Dev-hopping"],
+        YouTube: "",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/52283efe-be21-4000-b5c8-bab134e2cd92-profile_image-70x70.png",
+        description: "Cafetería central para streamers,Padre de Tanukis, trasteador de código sin saber muy bien que hace",
+    }
+    ,{
+        name: "charlyautomatiza",
+        web:"https://charlyautomatiza.vercel.app/",
+        GitHub:"https://github.com/charlyautomatiza/",
+        specialty: ["Testing","Automatización"],
+        YouTube: "https://www.youtube.com/channel/UCwEb6xrQtQCEuN_gNgi_Xfg",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/489f483d-7347-470d-b34a-3c4383719a88-profile_image-150x150.png",
+        description: "",
+    },{
+        name: "vamoacodear",
+        web:"https://vamoacodear.com.ar/",
+        GitHub:"https://github.com/nsdonato",
+        specialty: ["Frontend"],
+        YouTube: "https://www.youtube.com/@vamoacodear",
+        avatar:"./",
+        description: "",
+    },{
+        name:"niv3k_el_pato",
+        web:"https://elpatobot.com/",
+        GitHub:"https://github.com/Niv3K-El-Pato",
+        specialty: ["Desarrollo web","react","microservicios","azure","c#"],
+        YouTube: "",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/ef269242-c851-4d0b-b9db-0d959df33e1d-profile_image-70x70.png",
+        description: "Invasor de la tierra, vengador de patos y master de *quack*",
+    }
+    ,{
+        name: "malpicado",
+        web:"",
+        GitHub:"https://github.com/malpicado",
+        specialty: ["Desarrollo web"],
+        YouTube: "",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/07412663-1350-4828-b613-63daf943dcc4-profile_image-70x70.png",
+        description: "",
+    }
+    ,{
+        name: "manzdev",
+        web:"https://manz.dev/",
+        GitHub:"https://github.com/manzdev",
+        specialty: ["Desarrollo web", "frontend"],
+        YouTube: "https://www.youtube.com/@ManzDev",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/86bd7a3b-b42f-4463-a428-e3f8d0614208-profile_image-150x150.png",
+        description: "San CSS",
+    },{
+        name: "padawanstrainer",
+        web:"",
+        GitHub:"",
+        specialty: ["Desarrollo web", "Backend","PHP"],
+        YouTube: "",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/c00c87b1-b8b2-4b60-b6be-6f83a8c76c3c-profile_image-70x70.png",
+        description: "",
+    },{
+        name: "nurialiano",
+        web:"https://nurialiano.es/",
+        GitHub:"https://github.com/NuriaLiano",
+        specialty: ["Desarrollo web", "Backend","PHP","Sistemas","Comer y dormir"],
+        YouTube: "",
+        avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/c00c87b1-b8b2-4b60-b6be-6f83a8c76c3c-profile_image-70x70.png",
+        description: "sysadmin de plastilina",
+    }
+]
