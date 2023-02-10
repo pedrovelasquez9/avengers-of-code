f00214fd9652814ed96841dd1197e5664dac23eb
Pedro Plasencia <programacion.en.esp@gmail.com>
2023-02-10 09:34:28 +0100

    feat: removed unused components to avoid duplication in layout

--- src/components/ReactCard/ReactCard.tsx
+++ src/components/ReactCard/ReactCard.tsx
@@ -1,7 +1,11 @@
+import "./index.css";
+
 export const ReactCard = ({ devInfo }: any) => {
+  console.log(devInfo);
   return (
     <div className="card-container">
       <div className="info-container">
+        <img src={devInfo.avatar} alt="" />
         <h1>{devInfo.name}</h1>
         <p>{devInfo.description}</p>
       </div>
--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 import { streamers } from "../../data/stremers";
 import { ReactControls } from "../ReactControls/ReactControls";
 import { ReactCard } from "./../ReactCard/ReactCard";
+import "./index.css";
 
 export const Slide = () => {
   const [dev, setDev] = useState({});
-  
 
   return (
     <>
       <ReactCard devInfo={dev} />
-      {streamers.map((dev) => {
-        return <ReactControls handler={() => setDev(dev)} />;
-      })}
+      <div className="controls-container">
+        {streamers.map((dev) => {
+          return (
+            <ReactControls key={Math.random()} handler={() => setDev(dev)} />
+          );
+        })}
+      </div>
     </>
   );
 };
