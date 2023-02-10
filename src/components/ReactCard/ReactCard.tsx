a5b0b874d25eb621c058f4b78b9177df77595566
altaskur <altaskur@gmai.com>
2023-02-10 23:05:35 +0100

    add some style

--- src/components/ReactCard/ReactCard.tsx
+++ src/components/ReactCard/ReactCard.tsx
@@ -1,13 +1,36 @@
 import "./index.css";
 
 export const ReactCard = ({ devInfo }: any) => {
-  console.log(devInfo);
   return (
     <div className="card-container">
       <div className="info-container">
-        <img src={devInfo.avatar} alt="" />
-        <h1>{devInfo.name}</h1>
-        <p>{devInfo.description}</p>
+        <h2>{devInfo.name}</h2>
+        <p>
+        {devInfo.specialty.map((specialty:string) => {
+          return (`
+           <span className="green">[</span>
+           <span>"${specialty}"</span>
+           <span className="green">]</span>`
+          );
+        })}
+        </p>
+        <ul>
+          <li className="icon">
+            <a href={devInfo.GitHub} target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-github"></i>
+            </a>
+          </li>
+          <li className="icon">
+            <a href={devInfo.YouTube} target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-youtube"></i>
+            </a>
+          </li>
+          <li className="icon">
+            <a href={devInfo.Web} target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-globe"></i>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
